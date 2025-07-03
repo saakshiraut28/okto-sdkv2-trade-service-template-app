6825fe61d0e687c3c707f2896a42902f317519f6
Ovia <151402233+oviawork@users.noreply.github.com>
2025-07-03 11:40:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,22 +8,22 @@ export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss(), nodePolyfills()],
     // Only for Local Development
-    server: {
-      proxy: {
-        "/trade-stage-api-proxy": {
-          target: "https://okto-trade-service.oktostage.com",
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/trade-stage-api-proxy/, "/v1"),
-        },
-        "/trade-sandbox-api-proxy": {
-          target: "https://sandbox-okto-trade-service-kong.okto.tech/",
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/trade-sandbox-api-proxy/, "/v1"),
-        },
-      },
-    },
+    // server: {
+    //   proxy: {
+    //     "/trade-stage-api-proxy": {
+    //       target: "https://okto-trade-service.oktostage.com",
+    //       changeOrigin: true,
+    //       secure: false,
+    //       rewrite: (path) => path.replace(/^\/trade-stage-api-proxy/, "/v1"),
+    //     },
+    //     "/trade-sandbox-api-proxy": {
+    //       target: "https://sandbox-okto-trade-service-kong.okto.tech/",
+    //       changeOrigin: true,
+    //       secure: false,
+    //       rewrite: (path) => path.replace(/^\/trade-sandbox-api-proxy/, "/v1"),
+    //     },
+    //   },
+    // },
   };
 });
 
